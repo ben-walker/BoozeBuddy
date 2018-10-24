e212e4c231b2bca40e72d848bb0a35de3770f323
Ben Walker <bwalke05@gmail.com>
2018-10-24 13:09:04 -0400

    If user token already set, don't require login

--- screens/AuthLoadingScreen.js
+++ screens/AuthLoadingScreen.js
@@ -16,12 +16,11 @@ export default class AuthLoadingScreen extends React.Component {
 
   // Fetch the token from storage then navigate to our appropriate place
   _bootstrapAsync = async () => {
-    //const userToken = await AsyncStorage.getItem('userToken');
+    const userToken = await AsyncStorage.getItem('userToken');
 
     // This will switch to the App screen or Auth screen and this loading
     // screen will be unmounted and thrown away.
-    //this.props.navigation.navigate(userToken ? 'App' : 'Auth');
-    this.props.navigation.navigate('Auth');
+    this.props.navigation.navigate(userToken ? 'App' : 'Auth');
   };
 
   // Render any loading content that you like here
