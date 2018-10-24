dfafb971069b642254648c2ee61914e37ee652b6
Ben Walker <bwalke05@gmail.com>
2018-10-24 15:50:42 -0400

    Signup colour

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import {
     InputAccessoryView,
-    Button,
     Keyboard,
-    KeyboardAvoidingView,
-    AsyncStorage, StyleSheet,
+    AsyncStorage,
+    StyleSheet,
+    ScrollView,
 } from 'react-native';
 import {
   FormLabel,
   FormInput,
+  Button,
 } from 'react-native-elements';
 import colour from "../constants/Colors";
 
 export default class LoginScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Booze Buddy Signup',
+        headerTintColor: colour.defaultText,
+        headerStyle: {
+            backgroundColor: colour.dark
+        },
+    };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -25,88 +34,95 @@ export default class LoginScreen extends React.Component {
     this.signUp = this.signUp.bind(this)
   }
 
-  async signUp() {
-    const rawResponse = await fetch('https://dr-robotnik.herokuapp.com/api/signUp', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: this.state.username,
-        email: this.state.email,
-        gender: this.state.gender,
-        weightKg: this.state.weightKg,
-        password: this.state.password,
-      })
-    });
+    async signUp() {
+        const rawResponse = await fetch('https://dr-robotnik.herokuapp.com/api/signUp', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+                email: this.state.email,
+                gender: this.state.gender,
+                weightKg: this.state.weightKg,
+                password: this.state.password,
+            })
+        });
 
-    if (!rawResponse.ok) return alert("Signup failed.");
-    const response = await rawResponse.json();
+        if (!rawResponse.ok) return alert("Signup failed.");
+        const response = await rawResponse.json();
 
-    await AsyncStorage.setItem('userToken', JSON.stringify(response.user));
-    this.props.navigation.navigate('App');
-  }
+        await AsyncStorage.setItem('userToken', JSON.stringify(response.user));
+        this.props.navigation.navigate('App');
+    }
 
-  render() {
-    return (
-      <KeyboardAvoidingView
-        behavior='position'
-        keyboardVerticalOffset={65}
-      >
-        <FormLabel>USERNAME</FormLabel>
-        <FormInput
-          onChangeText={(username) => this.setState({username})}
-          value={this.state.username}
-          autoCapitalize='none'
-        />
+    render() {
+        return (
+            <ScrollView style={styles.container}>
+                <FormLabel>USERNAME</FormLabel>
+                <FormInput
+                    onChangeText={(username) => this.setState({username})}
+                    value={this.state.username}
+                    autoCapitalize='none'
+                    inputStyle={styles.input}
+                />
 
-        <FormLabel>EMAIL</FormLabel>
-        <FormInput
-          onChangeText={(email) => this.setState({email})}
-          value={this.state.email}
-          keyboardType='email-address'
-          autoCapitalize='none'
-        />
+                <FormLabel>EMAIL</FormLabel>
+                <FormInput
+                    onChangeText={(email) => this.setState({email})}
+                    value={this.state.email}
+                    keyboardType='email-address'
+                    autoCapitalize='none'
+                    inputStyle={styles.input}
+                />
 
-        <FormLabel>PASSWORD</FormLabel>
-        <FormInput
-          onChangeText={(password) => this.setState({password})}
-          value={this.state.password}
-          secureTextEntry
-          autoCapitalize='none'
-          placeholder='••••••••'
-        />
+                <FormLabel>PASSWORD</FormLabel>
+                <FormInput
+                    onChangeText={(password) => this.setState({password})}
+                    value={this.state.password}
+                    secureTextEntry
+                    autoCapitalize='none'
+                    placeholder='••••••••'
+                    placeholderTextColor='gray'
+                    inputStyle={styles.input}
+                />
 
-        <FormLabel>GENDER</FormLabel>
-        <FormInput
-          onChangeText={(gender) => this.setState({gender})}
-          value={this.state.gender}
-          placeholder='Male, Female, Other'
-        />
+                <FormLabel>GENDER</FormLabel>
+                <FormInput
+                    onChangeText={(gender) => this.setState({gender})}
+                    value={this.state.gender}
+                    placeholder='Male, Female, Other'
+                    placeholderTextColor='gray'
+                    inputStyle={styles.input}
+                />
 
-        <FormLabel>WEIGHT (KG)</FormLabel>
-        <FormInput
-          onChangeText={(weightKg) => this.setState({weightKg})}
-          value={this.state.weightKg}
-          keyboardType='decimal-pad'
-          inputAccessoryViewID='accessoryView'
-        />
+                <FormLabel>WEIGHT (KG)</FormLabel>
+                <FormInput
+                    onChangeText={(weightKg) => this.setState({weightKg})}
+                    value={this.state.weightKg}
+                    keyboardType='decimal-pad'
+                    inputAccessoryViewID='accessoryView'
+                    inputStyle={styles.input}
+                />
 
-        <InputAccessoryView nativeID='accessoryView'>
-          <Button
-            title='Done'
-            onPress={Keyboard.dismiss}
-          />
-        </InputAccessoryView>
+                <InputAccessoryView nativeID='accessoryView'>
+                    <Button
+                        title='Done'
+                        onPress={Keyboard.dismiss}
+                    />
+                </InputAccessoryView>
 
-        <Button
-          title='Sign Up'
-          onPress={this.signUp}
-        />
-      </KeyboardAvoidingView>
-    );
-  }
+                <Button
+                    onPress={this.signUp}
+                    style={styles.button}
+                    rounded
+                    title='Sign Up'
+                    backgroundColor={colour.accent}
+                />
+            </ScrollView>
+        );
+    }
 }
 const styles = StyleSheet.create({
     container: {
@@ -114,13 +130,18 @@ const styles = StyleSheet.create({
         backgroundColor: colour.background,
         paddingTop: 22
     },
+    input: {
+        color: 'white'
+    },
+    button: {
+        padding: 5
+    },
     contentContainer: {
         marginTop: 10,
         marginBottom:20,
         alignItems: 'center',
         backgroundColor:colour.secondary
     },
-
     imageIcon:{
         width:100,
         height:80,
