79ae823de446e6f63bbd4e61ab4136f887644637
Robyn Rintjema <robyn.rintjema@gmail.com>
2018-10-24 15:07:18 -0400

    home icon

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -23,8 +23,8 @@ HomeStack.navigationOptions = {
       focused={focused}
       name={
         Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
+          ? `ios-home${focused ? '' : '-outline'}`
+          : 'md-home'
       }
     />
   ),
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
 import colour from '../constants/Colors';
 import {AsyncStorage, Image, ScrollView, StyleSheet, View} from "react-native";
 import {Button} from "react-native-elements";
@@ -27,12 +26,10 @@ export default class SettingsScreen extends React.Component {
     }
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
     return (
         <View style={styles.container}>
             <ScrollView style={styles.container} >
-                
+
                 <Button
                     onPress={this.logOut}
                     style={styles.button}
