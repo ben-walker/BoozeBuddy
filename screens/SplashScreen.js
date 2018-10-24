5c313635f0068830ee0d9b63bd0dd6709ef0937b
gregorsharpe <gregorbsharpe@gmail.com>
2018-10-24 16:07:23 -0400

    Bringing the splash screen in line with the rest of our styling.

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -18,31 +18,33 @@ export default class SplashScreen extends React.Component {
 
     render() {
     return (
-      <View style={{flex: 1 , flexDirection:'column'}}>
-        <View style={{flex: 2, justifyContent: 'center', alignItems: 'center'}}>
-          <Text style={styles.titleText}> Booze Buddy </Text>
-        </View>
-        <View style={{flex: 2, flexDirection:'column', justifyContent: "space-around"}}>
-          <View>
-            <Button
-              style={styles.button}
-              rounded
-              title='Sign Up'
-              backgroundColor={colors.actionButton}
-              onPress={() => this.props.navigation.navigate('Signup')}
-              />
+      <View style={styles.container}>
+        <View style={{flex: 1 , flexDirection:'column'}}>
+          <View style={{flex: 2, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style={styles.titleText}> Booze Buddy </Text>
           </View>
-          <View>
-            <Button
-              style={styles.button}
-              rounded
-              title='Log In'
-              backgroundColor={colors.actionButton}
-              onPress={() => this.props.navigation.navigate('Login')}
-              />
+          <View style={{flex: 2, flexDirection:'column', justifyContent: "space-around"}}>
+            <View>
+              <Button
+                style={styles.button}
+                rounded
+                title='Sign Up'
+                backgroundColor={colors.actionButton}
+                onPress={() => this.props.navigation.navigate('Signup')}
+                />
+            </View>
+            <View>
+              <Button
+                style={styles.button}
+                rounded
+                title='Log In'
+                backgroundColor={colors.actionButton}
+                onPress={() => this.props.navigation.navigate('Login')}
+                />
+            </View>
           </View>
-        </View>
-        <View style={{flex: 1}}>
+          <View style={{flex: 1}}>
+          </View>
         </View>
       </View>
     );
@@ -54,8 +56,14 @@ const styles = StyleSheet.create({
     width: '50%',
     padding: 15,
   },
+  container: {
+      flex: 1,
+      backgroundColor: colors.background,
+      paddingTop: 22
+  },
   titleText: {
     fontSize: 30,
     fontWeight: 'bold',
+    color: colors.defaultText,
   },
 });
