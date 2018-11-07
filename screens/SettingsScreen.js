17ca7baf6e5ad77786455cac0f1a6655b220e207
Ben Walker <bwalke05@gmail.com>
2018-11-07 14:24:12 -0500

    Fix eslint issues

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -9,45 +9,39 @@ import colors from '../constants/Colors';
 import style from '../constants/StyleSheet';
 
 export default class SettingsScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.logOut = this.logOut.bind(this);
+  static navigationOptions = {
+    title: 'Settings',
+    headerTintColor: colors.defaultText,
+    headerStyle: { backgroundColor: colors.dark },
+  };
+
+  logOut = async () => {
+    const { navigation } = this.props;
+    await fetch('https://dr-robotnik.herokuapp.com/api/logOut', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    await AsyncStorage.removeItem('userToken');
+    navigation.navigate('Auth');
   }
 
-    static navigationOptions = {
-      title: 'Settings',
-      headerTintColor: colors.defaultText,
-      headerStyle: { backgroundColor: colors.dark },
-    };
-
-    async logOut() {
-      await fetch('https://dr-robotnik.herokuapp.com/api/logOut', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-      });
-      await AsyncStorage.removeItem('userToken');
-      this.props.navigation.navigate('Auth');
-    }
-
-    render() {
-      return (
-        <View style={style.container}>
-          <ScrollView style={style.container}>
-
-            <Button
-              onPress={this.logOut}
-              style={style.button}
-              rounded
-              title="Log Out"
-              backgroundColor={colors.errorBackground}
-            />
-          </ScrollView>
-        </View>
-
-      );
-    }
+  render() {
+    return (
+      <View style={style.container}>
+        <ScrollView style={style.container}>
+          <Button
+            onPress={this.logOut}
+            style={style.button}
+            rounded
+            title="Log Out"
+            backgroundColor={colors.errorBackground}
+          />
+        </ScrollView>
+      </View>
+    );
+  }
 }
