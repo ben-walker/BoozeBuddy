4e879b887c4ed5ca1d5829a53f732eb106f4b956
Ben Walker <bwalke05@gmail.com>
2018-11-07 16:43:44 -0500

    Remove unused import

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 import colour from '../constants/Colors';
