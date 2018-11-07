da23788863e047d9950e408abfbfd505817a8b49
Ben Walker <bwalke05@gmail.com>
2018-11-07 16:03:43 -0500

    Fix eslint errors

--- components/DrinkModal.js
+++ components/DrinkModal.js
@@ -7,107 +7,116 @@ import {
   Icon,
 } from 'react-native-elements';
 import Modal from 'react-native-modal';
-import DropDownAlert from 'react-native-dropdownalert';
+import DropdownAlert from 'react-native-dropdownalert';
 import style from '../constants/StyleSheet';
 import colors from '../constants/Colors';
+import * as cocktailIcon from '../assets/images/DrinkIcons/cocktail.png';
 
 class DrinkModal extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isVisible: false,
-      internalDrink: props.drinkData,
-    };
+    this.state = { isVisible: false };
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.drinkData !== this.props.drinkData) this.setState({ internalDrink: this.props.drinkData });
+  addToFavourites = async () => {
+    const { drinkData } = this.props;
+    const rawResponse = await fetch('https://dr-robotnik.herokuapp.com/api/addFavourite', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ lcboId: drinkData.lcbo_id }),
+    });
+
+    if (!rawResponse.ok) return this.dropDown.alertWithType('error', 'Error', 'Sorry, we couldn\'t add that drink to your Favourites :(');
+    return this.dropDown.alertWithType('success', 'Added to Favourites', `Nice! We just added ${drinkData.name} to your Favourites!`);
   }
 
-    _addToFavourites = async () => {
-      const drink = this.state.internalDrink;
-      const rawResponse = await fetch('https://dr-robotnik.herokuapp.com/api/addFavourite', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ lcboId: drink.lcbo_id }),
-      });
-
-      if (!rawResponse.ok) return this.dropDown.alertWithType('error', 'Error', 'Sorry, we couldn\'t add that drink to your Favourites :(');
-      this.dropDown.alertWithType('success', 'Added to Favourites', `Nice! We just added ${drink.name} to your Favourites!`);
+  getImageSrc = () => {
+    const { drinkData } = this.props;
+    if (drinkData) {
+      return drinkData.image_url
+        ? ({ uri: drinkData.image_url })
+        : cocktailIcon.default;
     }
+    return ({ uri: '' });
+  }
 
-    toggleModal = () => this.setState({ isVisible: !this.state.isVisible });
+  isDrinkFavourite = () => {
+    const { drinkData } = this.props;
+    return drinkData ? !!drinkData.favourite : false;
+  }
 
-    render() {
-      const addToFavouritesButton = (
-        <Button
-          title="Add to Favourites"
-          rounded
-          raised
-          backgroundColor={colors.background}
-          onPress={this._addToFavourites}
-        />
-      );
+  toggleModal = () => this.setState(prevState => ({ isVisible: !prevState.isVisible }))
 
-      const heartIcon = (
-        <Icon
-          name="favorite"
-          size={35}
-          color="red"
-        />
-      );
+  render() {
+    const { isVisible } = this.state;
 
-      return (
-        <Modal
-          isVisible={this.state.isVisible}
-          onSwipe={this.toggleModal}
-          swipeDirection="down"
-          style={style.drinkModal}
+    const addToFavouritesButton = (
+      <Button
+        title="Add to Favourites"
+        rounded
+        raised
+        backgroundColor={colors.background}
+        onPress={this.addToFavourites}
+      />
+    );
+
+    const heartIcon = (
+      <Icon
+        name="favorite"
+        size={35}
+        color="red"
+      />
+    );
+
+    const image = this.getImageSrc();
+
+    return (
+      <Modal
+        isVisible={isVisible}
+        onSwipe={this.toggleModal}
+        swipeDirection="down"
+        style={style.drinkModal}
+      >
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+          }}
         >
-          <View
-            style={{
-              flex: 1,
-              alignItems: 'center',
-            }}
-          >
-            <Tile
-              imageSrc={this.state.internalDrink
-                ? this.state.internalDrink.image_url
-                  ? { uri: this.state.internalDrink.image_url }
-                  : require('../assets/images/DrinkIcons/cocktail.png')
-                : { uri: '' }}
-              height={600}
-              width={400}
-            />
+          <Tile
+            imageSrc={image}
+            height={600}
+            width={400}
+          />
 
-            {this.state.internalDrink
-              ? this.state.internalDrink.favourite ? heartIcon : addToFavouritesButton
-              : null}
+          {this.isDrinkFavourite() ? heartIcon : addToFavouritesButton}
 
-            <Icon
-              name="arrow-downward"
-              raised
-              reverse
-              size={24}
-              onPress={this.toggleModal}
-              color="grey"
-            />
+          <Icon
+            name="arrow-downward"
+            raised
+            reverse
+            size={24}
+            onPress={this.toggleModal}
+            color="grey"
+          />
 
-            <DropDownAlert
-              ref={ref => this.dropDown = ref}
-            />
-          </View>
-        </Modal>
-      );
-    }
+          <DropdownAlert ref={(ref) => { this.dropdown = ref; }} />
+        </View>
+      </Modal>
+    );
+  }
 }
 
+DrinkModal.defaultProps = {
+  drinkData: null,
+};
+
 DrinkModal.propTypes = {
-  drinkData: PropTypes.object,
+  drinkData: PropTypes.object, // eslint-disable-line react/forbid-prop-types
 };
 
 export default DrinkModal;
