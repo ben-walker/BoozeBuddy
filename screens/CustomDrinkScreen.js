2dc4ddc830c9b46c1a14e1bd0d08ca61433f8323
Ben Walker <bwalke05@gmail.com>
2018-11-07 14:54:22 -0500

    Fix yet more eslint errors

--- screens/CustomDrinkScreen.js
+++ screens/CustomDrinkScreen.js
@@ -1,151 +1,170 @@
 import React from 'react';
 import {
-  InputAccessoryView,
-  Keyboard,
-  AsyncStorage,
   ScrollView,
-  Button as RawButton,
-  Platform,
   View,
 } from 'react-native';
 import {
+  Button,
   FormLabel,
   FormInput,
   FormValidationMessage,
-  CheckBox,
-  Text,
 } from 'react-native-elements';
-import { Button } from 'react-native-elements';
+import DropdownAlert from 'react-native-dropdownalert';
 import colors from '../constants/Colors';
 import style from '../constants/StyleSheet';
 import validate from '../utilities/validateWrapper';
-import DrinkCard from '../components/DrinkCard.js';
 
 
 export default class CustomDrinkScreen extends React.Component {
-    static navigationOptions = {
-      title: 'Create a drink',
-      headerTintColor: colors.defaultText,
-      headerStyle: { backgroundColor: colors.dark },
+  static navigationOptions = {
+    title: 'Create a drink',
+    headerTintColor: colors.defaultText,
+    headerStyle: { backgroundColor: colors.dark },
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      drinkName: '',
+      drinkNameError: '',
+      drinkVolume: '',
+      drinkVolumeError: '',
+      drinkAlcoholContent: '',
+      drinkAlcoholContentError: '',
     };
+  }
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        drinkName: '',
-        drinkNameError: '',
-        drinkVolume: '',
-        drinkVolumeError: '',
-        drinkAlcoholContent: '',
-        drinkAlcoholContentError: '',
-      };
-      this.createDrink = this.createDrink.bind(this);
+  createDrink = async () => {
+    const {
+      drinkName,
+      drinkVolume,
+      drinkAlcoholContent,
+    } = this.state;
+    const { navigation } = this.props;
+
+    if (!await this.isValid()) return;
+    const rawResponse = await fetch('https://dr-robotnik.herokuapp.com/api/createDrink', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: drinkName,
+        volumeInMilliliters: drinkVolume,
+        alcoholContent: drinkAlcoholContent,
+      }),
+    });
+
+    if (!rawResponse.ok) {
+      this.dropdown.alertWithType('error', 'Error', 'Drink creation failed.');
+      return;
     }
+    this.dropdown.alertWithType('success', 'Drink Created', `Successfully created ${drinkName}.`);
+    navigation.navigate('App');
+  }
 
-    async createDrink() {
-      if (!await this.isValid()) return;
-      const rawResponse = await fetch('https://dr-robotnik.herokuapp.com/api/createDrink', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name: this.state.drinkName,
-          volumeInMilliliters: this.state.drinkVolume,
-          alcoholContent: this.state.drinkAlcoholContent,
-        }),
-      });
+  async isValid() {
+    const {
+      drinkName,
+      drinkVolume,
+      drinkAlcoholContent,
+    } = this.state;
+    const drinkNameError = await validate('drinkName', drinkName);
+    const drinkVolumeError = await validate('drinkVolume', drinkVolume);
+    const drinkAlcoholContentError = await validate('drinkAlcoholContent', drinkAlcoholContent);
 
-      if (!rawResponse.ok) return alert('createDrink failed.');
-      const response = await rawResponse.json();
-      console.log(response);
-      alert(`Successfully created ${this.state.drinkName}.`);
-      this.props.navigation.navigate('App');
-    }
+    this.setState({
+      drinkNameError,
+      drinkVolumeError,
+      drinkAlcoholContentError,
+    });
 
-    async isValid() {
-      const drinkNameError = await validate('drinkName', this.state.drinkName);
-      const drinkVolumeError = await validate('drinkVolume', this.state.drinkVolume);
-      const drinkAlcoholContentError = await validate('drinkAlcoholContent', this.state.drinkAlcoholContent);
+    return new Promise((resolve) => {
+      resolve(!drinkNameError && !drinkVolumeError && !drinkAlcoholContentError);
+    });
+  }
 
-      this.setState({
-        drinkNameError,
-        drinkVolumeError,
-        drinkAlcoholContentError,
-      });
+  render() {
+    const {
+      drinkName,
+      drinkNameError,
+      drinkVolume,
+      drinkVolumeError,
+      drinkAlcoholContent,
+      drinkAlcoholContentError,
+    } = this.state;
 
-      return new Promise((resolve) => {
-        resolve(!drinkNameError && !drinkVolumeError && !drinkAlcoholContentError);
-      });
-    }
+    return (
+      <View style={style.container}>
+        <ScrollView style={style.container} contentContainerStyle={style.secondaryContentContainer}>
 
-    render() {
-      return (
-        <View style={style.container}>
-          <ScrollView style={style.container} contentContainerStyle={style.secondaryContentContainer}>
+          <FormLabel>Drink Name</FormLabel>
+          <FormInput
+            onChangeText={drinkNameInput => this.setState({ drinkName: drinkNameInput })}
+            value={drinkName}
+            autoCapitalize="none"
+            inputStyle={style.input}
+            onBlur={async () => {
+              this.setState({
+                drinkNameError: await validate('drinkName', drinkName),
+              });
+            }}
+          />
+          <FormValidationMessage labelStyle={style.errorMsg}>
+            {drinkNameError}
+          </FormValidationMessage>
 
-            <FormLabel>Drink Name</FormLabel>
-            <FormInput
-              onChangeText={drinkName => this.setState({ drinkName })}
-              value={this.state.drinkName}
-              autoCapitalize="none"
-              inputStyle={style.input}
-              onBlur={async () => {
-                this.setState({
-                  drinkNameError: await validate('drinkName', this.state.drinkName),
-                });
-              }}
-            />
-            <FormValidationMessage labelStyle={style.errorMsg}>
-              {this.state.drinkNameError}
-            </FormValidationMessage>
+          <FormLabel>Drink Volume (ml)</FormLabel>
+          <FormInput
+            onChangeText={drinkVolumeInput => this.setState({ drinkVolume: drinkVolumeInput })}
+            value={drinkVolume}
+            keyboardType="decimal-pad"
+            inputAccessoryViewID="accessoryView"
+            inputStyle={style.input}
+            onBlur={async () => {
+              this.setState({
+                drinkVolumeError: await validate('drinkVolume', drinkVolume),
+              });
+            }}
+          />
+          <FormValidationMessage labelStyle={style.errorMsg}>
+            {drinkVolumeError}
+          </FormValidationMessage>
 
-            <FormLabel>Drink Volume (ml)</FormLabel>
-            <FormInput
-              onChangeText={drinkVolume => this.setState({ drinkVolume })}
-              value={this.state.drinkVolume}
-              keyboardType="decimal-pad"
-              inputAccessoryViewID="accessoryView"
-              inputStyle={style.input}
-              onBlur={async () => {
-                this.setState({
-                  drinkVolumeError: await validate('drinkVolume', this.state.drinkVolume),
-                });
-              }}
-            />
-            <FormValidationMessage labelStyle={style.errorMsg}>
-              {this.state.drinkVolumeError}
-            </FormValidationMessage>
+          <FormLabel>Alcohol Content (%)</FormLabel>
+          <FormInput
+            onChangeText={
+              drinkAlcoholContentInput => this.setState({
+                drinkAlcoholContent: drinkAlcoholContentInput,
+              })
+            }
+            value={drinkAlcoholContent}
+            keyboardType="decimal-pad"
+            inputAccessoryViewID="accessoryView"
+            inputStyle={style.input}
+            onBlur={async () => {
+              this.setState({
+                drinkAlcoholContentError: await validate('drinkAlcoholContent', drinkAlcoholContent),
+              });
+            }}
+          />
+          <FormValidationMessage labelStyle={style.errorMsg}>
+            {drinkAlcoholContentError}
+          </FormValidationMessage>
 
-            <FormLabel>Alcohol Content (%)</FormLabel>
-            <FormInput
-              onChangeText={drinkAlcoholContent => this.setState({ drinkAlcoholContent })}
-              value={this.state.drinkAlcoholContent}
-              keyboardType="decimal-pad"
-              inputAccessoryViewID="accessoryView"
-              inputStyle={style.input}
-              onBlur={async () => {
-                this.setState({
-                  drinkAlcoholContentError: await validate('drinkAlcoholContent', this.state.drinkAlcoholContent),
-                });
-              }}
-            />
-            <FormValidationMessage labelStyle={style.errorMsg}>
-              {this.state.drinkAlcoholContentError}
-            </FormValidationMessage>
+          <Button
+            onPress={this.createDrink}
+            style={style.button}
+            rounded
+            title="Create"
+            backgroundColor={colors.accent}
+          />
 
-            <Button
-              onPress={this.createDrink}
-              style={style.button}
-              rounded
-              title="Create"
-              backgroundColor={colors.accent}
-            />
-
-          </ScrollView>
-        </View>
-      );
-    }
+        </ScrollView>
+        <DropdownAlert ref={(ref) => { this.dropdown = ref; }} />
+      </View>
+    );
+  }
 }
