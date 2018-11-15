065a604e5777682ec0f127ebe69ef17914fde39c
Ben Walker <bwalke05@gmail.com>
2018-11-15 12:10:24 -0500

    Pause the preview on take picture

--- components/CameraModal.js
+++ components/CameraModal.js
@@ -23,6 +23,7 @@ class CameraModal extends Component {
   recordPicture = async () => {
     const { onNewImage } = this.props;
     if (this.camera) {
+      this.camera.pausePreview();
       const photo = await this.camera.takePictureAsync({ base64: true });
       onNewImage(photo);
       this.toggleModal();
