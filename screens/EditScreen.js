c9cc908806bfa8aa31ecfdb1cc85ca238c898305
Robyn Rintjema <robyn.rintjema@gmail.com>
2018-11-27 22:45:09 -0500

    Update profile info

--- screens/EditScreen.js
+++ screens/EditScreen.js
@@ -5,104 +5,140 @@ import {
   ScrollView,
   View,
 } from 'react-native';
-import { Button, List, ListItem } from 'react-native-elements';
+import {
+  Button, FormInput, FormLabel,
+} from 'react-native-elements';
+import PickerSelect from 'react-native-picker-select';
 import colors from '../constants/Colors';
 import style from '../constants/StyleSheet';
 
 export default class EditScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Edit',
-    headerTintColor: colors.defaultText,
-    headerStyle: { backgroundColor: colors.dark },
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      userdata: '',
+    static navigationOptions = {
+      title: 'Edit',
+      headerTintColor: colors.defaultText,
+      headerStyle: { backgroundColor: colors.dark },
     };
-  }
 
-  async componentDidMount() {
-    // parse out user data
-    const userData = await AsyncStorage.getItem('userToken').then(userToken => JSON.parse(userToken));
-    this.setState({ userdata: userData });
-  }
+    constructor(props) {
+      super(props);
+      this.state = {
+        userdata: '',
+          gender: '',
+          weightKg: ''
+      };
+    }
 
-  save = async () => {
-    const { navigation } = this.props;
-    await fetch('https://dr-robotnik.herokuapp.com/api/logOut', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    });
-    await AsyncStorage.removeItem('userToken');
-    navigation.navigate('Auth');
-  };
+    async componentDidMount() {
+      // parse out user data
+      const userData = await AsyncStorage.getItem('userToken').then(userToken => JSON.parse(userToken));
+      this.setState({ userdata: userData});
+      this.setState({
+          gender: userData.gender,
+          weightKg: userData.weightKg.toString()});
+    }
 
-  render() {
-    const { navigation } = this.props;
-    const { userdata } = this.state;
-    const list = [
-      {
-        title: 'Gender',
-        subtitle: userdata.gender,
+    save = async () => {
+      const { navigation } = this.props;
+        let rawResponse;
+
+      rawResponse = await fetch('https://dr-robotnik.herokuapp.com/api/updateData', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+
+        },
+        body: JSON.stringify({ gender: this.state.gender, weightKg: this.state.weightKg*1 }),
+      });
+      alert(rawResponse.ok);
+      navigation.navigate('Profile');
+    };
+
+    render() {
+      const { navigation } = this.props;
+      const {
+        userdata,
+        gender,
+        theme,
+        weightKg,
+      } = this.state;
 
 
-      },
-      {
-        title: 'Weight',
-        subtitle: userdata.weightKg,
+
+      return (
+        <View style={style.container}>
+          <ScrollView style={style.container}>
+            <FormLabel>GENDER</FormLabel>
+            <PickerSelect
+              items={[
+                { label: 'Male', value: 'Male' },
+                { label: 'Female', value: 'Female' },
+                { label: 'Other', value: 'Other' },
+              ]}
+
+              onValueChange={async (value) => {
+                await this.setState({ gender: value });
+                userdata.gender = value;
+              }}
+              hideDoneBar
+            >
+              <FormInput
+                value={gender}
+                inputStyle={style.input}
+              />
+            </PickerSelect>
 
 
-      },
-      {
-        title: 'Theme',
-        subtitle: 'Dark',
+            <FormLabel>WEIGHT (KG)</FormLabel>
+            <FormInput
+              onChangeText={async (weightKgInput) => {
+                this.setState({ weightKg: weightKgInput });
+              }}
+              value={weightKg}
+              keyboardType="decimal-pad"
+              inputAccessoryViewID="accessoryView"
+              inputStyle={style.input}
 
+            />
 
-      },
+            <FormLabel>Theme</FormLabel>
+            <PickerSelect
+              items={[
+                { label: 'Dark', value: 'Dark' },
+                { label: 'Light', value: 'Light' },
+              ]}
+              onValueChange={async (value) => {
+                await this.setState({ theme: value });
+              }}
+              hideDoneBar
+            >
+              <FormInput
+                value={theme}
+                inputStyle={style.input}
+              />
+            </PickerSelect>
 
-
-    ];
-
-    return (
-      <View style={style.container}>
-        <ScrollView style={style.container}>
-          <List>
-            {
-                    list.map(item => (
-                      <ListItem
-                        key={item.title}
-                        title={item.title}
-                        subtitle={item.subtitle}
-                      />
-                    ))
-                }
-          </List>
-          <Button
-            onPress={this.save}
-            containerViewStyle={style.button}
-            rounded
-            raised
-            title="Save"
-            backgroundColor={colors.errorBackground}
-          />
-          <Button
-            onPress={() => navigation.navigate('Profile')}
-            containerViewStyle={style.button}
-            rounded
-            raised
-            title="Cancel"
-            backgroundColor={colors.accent}
-          />
-        </ScrollView>
-      </View>
-    );
-  }
+            <Button
+              onPress={this.save}
+              containerViewStyle={style.button}
+              rounded
+              raised
+              title="Save"
+              backgroundColor={colors.errorBackground}
+            />
+            <Button
+              onPress={() => navigation.navigate('Profile')}
+              containerViewStyle={style.button}
+              rounded
+              raised
+              title="Cancel"
+              backgroundColor={colors.accent}
+            />
+          </ScrollView>
+        </View>
+      );
+    }
 }
 
 EditScreen.propTypes = {
